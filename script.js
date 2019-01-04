64f11d91fad1f3c744c155477c52b4b51f4eed1e
zbych89 <zbych89@gmail.com>
2019-01-04 22:01:22 +0100

    19.1 App update attempt2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://serene-shore-16749.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://serene-shore-16749.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
